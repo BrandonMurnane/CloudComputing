495d0904cceb58547f743bc7da6190a81dd0575a
BrandonMurnane <bran.999@hotmail.com>
2015-09-30 15:25:05 +0000

    finish

--- Lab3/palindrome.py
+++ Lab3/palindrome.py
@@ -1,28 +1,24 @@
-string1="Oxo"
-string2="OXO"
-string3="12345321"
-string4="ROTATOR"
-string5="12345 54321"
-
+strings=["Oxo","OXO","12345321","ROTATOR","12345 54321"]
 
 def del_space(word):
     word = word.replace(" ","")
     word = word.lower()
     print(word)
     return word
-  
+
 def rev_word(word):
     reversed_word = word[::-1]
     print(reversed_word)
     return reversed_word
-  
-del_space(string1)
-rev_word(string1)
-del_space(string2)
-rev_word(string2)
-del_space(string3)
-rev_word(string3)
-del_space(string4)
-rev_word(string4)
-del_space(string5)
-rev_word(string5)
+
+
+for word in strings:
+  if(rev_word(word) == del_space(word)):
+      print(reversed_word + " AND " + del_space)
+      print("Palindrome")
+      break;
+  else:
+      print("Not a palindrome")
+
+
+
