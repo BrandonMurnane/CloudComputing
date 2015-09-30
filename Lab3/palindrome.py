7e7b63dadeea21d2faa3c0ef08fa12b85c2484ff
BrandonMurnane <bran.999@hotmail.com>
2015-09-30 15:06:12 +0000

    next bit next bit

--- Lab3/palindrome.py
+++ Lab3/palindrome.py
@@ -1,6 +1,28 @@
-check=input("input word for testing")
+string1="Oxo"
+string2="OXO"
+string3="12345321"
+string4="ROTATOR"
+string5="12345 54321"
+
+
 def del_space(word):
     word = word.replace(" ","")
     word = word.lower()
     print(word)
     return word
+  
+def rev_word(word):
+    reversed_word = word[::-1]
+    print(reversed_word)
+    return reversed_word
+  
+del_space(string1)
+rev_word(string1)
+del_space(string2)
+rev_word(string2)
+del_space(string3)
+rev_word(string3)
+del_space(string4)
+rev_word(string4)
+del_space(string5)
+rev_word(string5)
